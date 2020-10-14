9b6d4a4faded2f76523c36c127fd749c6784eba3
ManaliParab12 <manaliparab10@gmail.com>
2020-10-14 07:13:41 +0530

    First Name With Capital and Minimum 3 Characters

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration"
+
+function validFirstName ()
+{
+   echo "Enter your First Name "
+   read  fName
+
+   pattern="^[A-Z]{1}[a-z]{2,10}$"
+
+   if [[ $fName =~ $pattern ]]
+   then
+       echo "First Name is Valid"
+   else
+       echo "First Name is Invalid"
+   fi
+}
+
+validFirstName
